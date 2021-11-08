0a3cb224d977723e1e18120a402ce849fea03be9
Nazar Fisenko <49079058+FisenkoN@users.noreply.github.com>
2021-11-08 06:55:16 -0500

    fix

---
+++ School.WEB/Extensions/OperationResult.cs
@@ -0,0 +1,25 @@
+// using Calabonga.OperationResults;
+//
+// namespace School.WEB.Extensions
+// {
+//     public class Operation
+//     {
+//         protected OperationResult GetOperationResult()
+//         {
+//             return TempData.Get<OperationResult>(OperationResultKey);
+//         }
+//
+//         protected void SetOperationResult(OperationResult model)
+//         {
+//             TempData.Put(OperationResultKey,
+//                 model);
+//         }
+//
+//         protected void SetOperationResult(bool isSuccess, string message)
+//         {
+//             var operationResult = new OperationResult(
+//             SetOperationResult(operationResult);
+//         }
+//
+//     }
+// }
